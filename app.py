021eab4fe5a01503e4ad51a286b91cb1e0536450
Parth Tiwari <99129984+realParthTiwari@users.noreply.github.com>
2025-07-25 22:56:52 +0530

    Setup flask application

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from routes.auth import auth_bp
+
+app = Flask(__name__)
+app.secret_key = 'your-secret-key'
+
+# Register Blueprints
+app.register_blueprint(auth_bp)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/select")
+def select():
+    return render_template("select.html")
+
+
+@app.route("/pose")
+def pose():
+    return render_template("pose.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

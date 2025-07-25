483a819627b7450e524f29bf7c965b1acd379854
Parth Tiwari <99129984+realParthTiwari@users.noreply.github.com>
2025-07-26 02:34:42 +0530

    Update app.py to enable authentication

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
-from routes.auth import auth_bp
+from routes.auth import auth_bp, config_oauth
+
 
 app = Flask(__name__)
 app.secret_key = 'your-secret-key'
 
+config_oauth(app)
 # Register Blueprints
 app.register_blueprint(auth_bp)
 
@@ -21,6 +23,7 @@ def select():
 @app.route("/pose")
 def pose():
     return render_template("pose.html")
+
 
 
 if __name__ == "__main__":
